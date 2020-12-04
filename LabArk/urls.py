99fcaacc38b4b6e790bd23b7e0ffb62e6bb3468d
Seboo <seboo@yandex.by>
2020-12-04 20:37:01 +0300

    add login\registration

--- LabArk/urls.py
+++ LabArk/urls.py
@@ -5,9 +5,12 @@ from LabArkApp import views as LA
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^faq', LA.faq, name="faq"),
-    re_path('', LA.home, name="homepage"),
     re_path('^popular', LA.popular, name="popular"),
     re_path('^libs', LA.libs, name="libs"),
     re_path('^archive', LA.archive, name="archive"),
     re_path('^links', LA.links, name="links"),
+    re_path('^register', LA.register, name="register"),
+    re_path('^login', LA.login_view, name="login"),
+    re_path('^logout', LA.logout_view, name="logout"),
+    re_path('', LA.home, name="homepage"),
 ]
--- LabArkApp/views.py
+++ LabArkApp/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def home(request):
@@ -23,3 +27,38 @@ def libs(request):
 
 def links(request):
     return render(request, 'links.html')
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/")
+            else:
+                pass
+        else:
+            pass
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+        return HttpResponseRedirect("/")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/")
